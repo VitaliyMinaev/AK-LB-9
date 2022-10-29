525f0b193a99e03128e489038933e1a06dae2fa5
Vitaliy <bezuglivita123@gmail.com>
2022-10-29 21:41:00 +0300

    Third task done

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -68,8 +68,6 @@ void Task2() {
 
         fstp testRes
 
-
-
         fld testRes                 ; push result to the FPU stack
         fld ONEHUNDRED              ; push ONEHUNDRED to the FPU stack
         fucomip st(0), st(1)        ; compare 100 > result
@@ -90,11 +88,95 @@ void Task2() {
 
     std::cout << "Result: " << testRes << " ; N: " << n << std::endl;
 }
+
+// 1/2 * arcCosec(3 * A^2 + 4 * B) => 1/2 * arcsin(1 / (3 * A^2 + 4 * B))
+void Task3() {
+    // Нехай A = 0, a B = 0.5
+    const double A = 0, B = 0.5, THREE = 3, ONEANDHALF = 0.5, 
+        FOUR = 4, ONE = 1, ONEHUNDREEDEIGHTEEN = 180, PI = 3.14159;
+    double resultInRadians, resultInDegrees, fourMultiplyB, arcCosecValue;
+
+    _asm
+    {
+        finit                   ; inits FPU
+
+        fld B                   ; st(0) = B
+        fmul FOUR               ; st(0) = 4 * B
+        fstp fourMultiplyB      ; fourMultiplyB = 4 * B => pop
+
+        fld A                   ; st(0) = A
+        fmul A                  ; st(0) = A * A
+        fmul THREE              ; st(0) = 3 * A * A
+
+        fadd fourMultiplyB      ; st(0) = 3 * A ^ 2 + 4 * B
+
+        fstp arcCosecValue      ; arcCosecValue = 3 * A ^ 2 + 4 * B => pop
+        
+        fld ONE                 ; st(0) = 1
+        fdiv arcCosecValue      ; st(0) = 1 / arcCosecValue
+
+        fld st                  ; arcSin operation
+        fld st
+        fld1
+        fsubr st(2), st
+        fadd
+        fmul
+        fsqrt
+        fpatan                   ; arcSin(1 / arcCosecValue) = arcCosec(arcCosecValue)
+
+        fstp resultInRadians     ; resultInRadians = arcSec(1 / arcCosecValue) = > pop
+
+        fld ONEHUNDREEDEIGHTEEN  ; st(0) = 180
+        fdiv PI                  ; st(0) = 180 / PI
+        fmul resultInRadians     ; st(0) = resultInRadians * 180/PI => degrees
+        
+        fmul ONEANDHALF          ; st(0) = 0.5 * st(0)
+
+        fstp resultInDegrees     ; resultInDegrees = st(0) => pop
+
+        fld resultInRadians
+        fmul ONEANDHALF          ; st(0) = 0.5 * resultInRadians
+        fstp resultInRadians     ; resultInRadians = 0.5 * resultInRadians = > pop
+    }
+
+    std::cout << "Result in radians: " << resultInRadians << std::endl
+        << "Result in degrees: " << resultInDegrees << std::endl;
+}
+
+void Test()
+{
+    double arcCosecValue = 0.5, result;
+
+    _asm
+    {
+        fld arcCosecValue
+        fld st
+        fld st
+        fld1
+        fsubr st(2), st
+        fadd
+        fmul
+        fsqrt
+        fpatan
+
+        ; fpatan; arcSec(1 / arcCosecValue)
+
+        fstp result; result = arcSec(1 / arcCosecValue) = > pop
+    }
+
+    std::cout << "Result: " << result << std::endl;
+}
+
 int main()
 {
     std::cout << "Task 1: " << std::endl;
     Task1();
+
     std::cout << std::endl << "Task 2: " << std::endl;
     Task2();
-    // std::cout << sqroot(42) << std::endl;
+
+    std::cout << std::endl << "Task 3: " << std::endl;
+    Task3();
+
+    return 0;
 }
